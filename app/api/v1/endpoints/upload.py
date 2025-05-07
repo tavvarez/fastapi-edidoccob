6a6b9ce32c4aa7e83c40ab561793edabbe21674b
tavvarez <gabrieldaraujotavares@gmail.com>
2025-05-07 19:03:38 -0300

    feat: importar multiplos xmls

--- app/api/v1/endpoints/upload.py
+++ app/api/v1/endpoints/upload.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, UploadFile, File, HTTPException, Depends;
 from sqlalchemy.orm import Session;
 from app.services.xml_parser import parse_cte_xml;
@@ -6,19 +7,26 @@ from app.core.database import get_db;
 
 router = APIRouter();
 @router.post("/upload-xml")
-async def upload_xml(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    if not file.filename.endswith('.xml'):
-        raise HTTPException(status_code=400, detail="Tipo de arquivo inválido. Apenas XML é permitido.");
-    try:
-        contents = await file.read();
-        with open(f"/tmp/{file.filename}", "wb") as f:
-            f.write(contents);
+async def upload_xml(files: List[UploadFile] = File(...), db: Session = Depends(get_db)):
+    cte_ids = []
+    for file in files:
+        if not file.filename.endswith('.xml'):
+            raise HTTPException(status_code=400, detail=f"Tipo de arquivo inválido. Apenas XML é permitido. {file.filename}")
+        try:
+            contents = await file.read()
+            temp_path = f"/tmp/{file.filename}"
+            with open(temp_path, "wb") as f:
+                f.write(contents)
 
-        dados = parse_cte_xml(f"/tmp/{file.filename}");
-        novo_cte = CteInfo(**dados);
-        db.add(novo_cte);
-        db.commit();
-        db.refresh(novo_cte);
-        return {"message": "XML processado e salvo", "cte_id": novo_cte.id};
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e));
+            dados = parse_cte_xml(temp_path)
+            novo_cte = CteInfo(**dados)
+            db.add(novo_cte)
+            db.flush()
+            cte_ids.append(novo_cte.id)
+            db.refresh(novo_cte)
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Erro ao processar {file.filename}: {str(e)}")
+    
+    db.commit()
+    return {"message": f"{len(cte_ids)} XMLs processados e salvos", "cte_ids": cte_ids}
