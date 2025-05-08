6a4ce7aaf10c26b49790b329b0effb7e1dccc279
tavvarez <gabrieldaraujotavares@gmail.com>
2025-05-08 17:24:26 -0300

    feat: adicionado nova tabela para relação de id de cte para geração de edi com multiplos xmls

--- app/models/user_input.py
+++ app/models/user_input.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func;
-from app.models.base import Base;
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+from app.models.base import Base
 
 class UserInput(Base):
-    __tablename__ = 'user_input';
+    __tablename__ = 'user_input'
 
-    id = Column(Integer, primary_key=True, index=True);
+    id = Column(Integer, primary_key=True, index=True)
     data_hora = Column(DateTime(timezone=True), server_default=func.now())
-    nome_cliente = Column(String(100), nullable=False);
-    numero_cte = Column(String(20), nullable=False);
-    numero_fatura = Column(String(20), nullable=False);
+    nome_cliente = Column(String(100), nullable=False)
+    # numero_cte = Column(String(20), nullable=False)
+    numero_fatura = Column(String(20), nullable=False)
 
     #cte_id = Column(Integer, ForeignKey('cte_info.id'), nullable=False);
--- app/schemas/user_input_schema.py
+++ app/schemas/user_input_schema.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel;
 from datetime import datetime;
+from typing import Optional, List
 
 class UserInputCreate(BaseModel):
-    nome_cliente: str;
-    numero_cte: str;
-    numero_fatura: str;
+    nome_cliente: str
+    numero_fatura: str
+    cte_ids: List[int]
 
-class UserInputOut(UserInputCreate):
-    id: int;
-    data_hora: datetime;
+class UserInputOut(BaseModel):
+    id: int
+    nome_cliente: str
+    numero_fatura: str
+    data_hora: datetime
+
 
     class Config:
-        orm_mode = True;
+        orm_mode = True
