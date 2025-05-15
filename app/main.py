6e1b50aa30547cb2048f05c095d59e3f276c5eb2
tavvarez <gabrieldaraujotavares@gmail.com>
2025-05-15 10:36:34 -0300

    theme: obsidian no swagger

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI;
-from app.api.v1.endpoints import upload, user_input, edi;
+from fastapi import FastAPI
+from app.api.v1.endpoints import upload, user_input, edi
 
-app = FastAPI(title="fastapi-edibialog");
-app.include_router(upload.router, prefix="/api/v1");
-app.include_router(user_input.router, prefix="/api/v1");
-app.include_router(edi.router, prefix="/api/v1");
+app = FastAPI(title="fastapi-edibialog", swagger_ui_parameters={"syntaxHighlight": {"theme": "obsidian"}})
+app.include_router(upload.router, prefix="/api/v1")
+app.include_router(user_input.router, prefix="/api/v1")
+app.include_router(edi.router, prefix="/api/v1")
